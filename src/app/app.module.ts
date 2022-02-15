5308ad119a321b09c653cd3c8cfe0cc294fbb158
Nahuel <nahuelbravo05@gmail.com>
2022-02-15 10:56:05 -0300

    modulos, fontawesome y BS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { SoftSkillsComponent } from './components/skills/soft-skills/soft-skills.component';
+import { HardSkillsComponent } from './components/skills/hard-skills/hard-skills.component';
+import { LogoutComponent } from './components/header/boton-login/logout/logout.component';
+import { BotonLoginLogoutComponent } from './components/header/boton-login-logout/boton-login-logout.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    SoftSkillsComponent,
+    HardSkillsComponent,
+    LogoutComponent,
+    BotonLoginLogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/boton-login-logout/boton-login-logout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-login-logout',
+  templateUrl: './boton-login-logout.component.html',
+  styleUrls: ['./boton-login-logout.component.css']
+})
+export class BotonLoginLogoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
